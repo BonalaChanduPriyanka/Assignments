cb44a46b8157d47ec20ce7cdd8814f830347afb3
BonalaChanduPriyanka <68299627+BonalaChanduPriyanka@users.noreply.github.com>
2020-07-27 21:19:02 +0530

    Add files via upload

---
+++ 4-assignment/program6-assgn4/shape/Polygon.java
@@ -0,0 +1,8 @@
+package com.shape;
+
+public interface Polygon {
+	
+	void calcArea(float Side );	//Method to calculate area 
+	void calcPeri( float length,float breadth);   //Method to calculate perimeter 
+
+}
